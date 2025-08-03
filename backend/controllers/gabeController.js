f00062cfba2f83c7f72ab4dede81caebb499d6d0
gabbykoms <gabrielkson15@gmail.com>
2025-08-03 18:52:07 -0400

    got rid of testing modules created earlier

--- backend/controllers/gabeController.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-
-const getGabeText = async (req, res) => {
-    
-    return res.send('Hello gabe, welcome to your page. Testing with postman');
-  };
-
-  module.exports = {getGabeText};
