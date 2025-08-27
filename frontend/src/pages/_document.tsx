f11ff2b22361bfffb0cad3c18ffe41bc350cdd08
clasona <yves.semanagisubizo@clasona.com>
2025-08-26 19:12:09 -0500

    frontend/src/pages/_document.tsx

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script defer data-domain={"test.vesoko.com"} src="https://analytics.vesoko.com/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              "window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }",
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
