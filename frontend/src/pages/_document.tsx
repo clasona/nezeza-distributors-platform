840820f4a538dc574b1f6aecd33d5e926ba715db
clasona <yves.semanagisubizo@clasona.com>
2025-08-28 21:56:00 -0500

    add google metatag

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="ICh9hq1y7fKfaVRc5sfzCUA_V0SBWJyCgfEKiK79J0o" />
         <script defer data-domain="vesoko.com,test.vesoko.com" src="https://analytics.vesoko.com/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"></script>
         <script
           dangerouslySetInnerHTML={{
